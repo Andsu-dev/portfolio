5554bd66b118d3af8d1f3bd6ec72c63bf9e272bc
AndersonVoeja <am@voeja.us>
2025-06-20 13:31:10 -0300

    feat: add interfaces for LinkedIn posts, navigation items, projects, and skills

---
+++ src/interfaces/linkedinPost.interface.ts
@@ -0,0 +1,9 @@
+export interface LinkedInPostProps {
+  id: string;
+  text: string;
+  imageUrl?: string;
+  likes?: number;
+  comments?: number;
+  timestamp: string;
+  postUrl: string;
+}
---
+++ src/interfaces/skill.interface.ts
@@ -0,0 +1,7 @@
+import type React from "react";
+
+export interface Skill {
+  icon?: React.ElementType;
+  name: string;
+  level?: string;
+}
